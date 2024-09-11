fa1556c5736d2d82db7fc00512cfa20964f80d73
hectorserna <hectorserna@RIAMPC05>
2024-09-11 08:40:42 +0200

    Agregar archivos de proyecto.

---
+++ RecoleccionDatos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+public class Record
+{
+    public int Year { get; set; }
+    public string ISOCode { get; set; }
+    public string Countries { get; set; }
+    public double EconomicFreedomIndex { get; set; }    
+    public int Rank { get; set; }
+
+}
+class Program
+{
+    static void Main()
+    {
+        // Ruta del archivo CSV
+        string filePath = "C:\\Users\\hectorserna\\Downloads\\economicdata2000-2021.csv";
+
+        // Verificar si el archivo existe
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("El archivo no existe.");
+            return;
+        }
+
+        // Leer el archivo CSV línea por línea
+        string line = File.ReadLines(filePath).ToArray()[4];
+        {
+            // Dividir la línea en campos usando la coma como delimitador
+            string[] fields = line.Split(',');
+
+            // Procesar los campos (aquí simplemente los imprimimos)
+            for (int i = 0; i < fields.Length; i++)
+            {
+                Console.WriteLine($"Campo {i + 1}: {fields[i]}");
+            }
+            Console.WriteLine(); // Nueva línea para separar registros
+        }
+    }
+}
